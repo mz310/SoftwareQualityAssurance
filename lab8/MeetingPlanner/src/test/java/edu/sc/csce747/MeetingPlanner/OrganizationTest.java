edee975644ecfe2ef9fb9028ce7fb9404bd8ba8e
Zorigt Munkhbaatar <mzorigt6@gmail.com>
2025-10-28 23:47:14 +0800

    Zuw uruunii dugaar esehiig shalgah mun ajilching

--- lab8/MeetingPlanner/src/test/java/edu/sc/csce747/MeetingPlanner/OrganizationTest.java
+++ lab8/MeetingPlanner/src/test/java/edu/sc/csce747/MeetingPlanner/OrganizationTest.java
@@ -1,9 +1,39 @@
 package edu.sc.csce747.MeetingPlanner;
 
-import  static org.junit.Assert.*;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class OrganizationTest {
-	// Add test methods here.
-    // You are not required to write tests for all classes.
+
+    @Test
+    public void testGetRoom_valid() throws Exception {
+        Organization org = new Organization();
+        Room r = org.getRoom("2A01");
+        assertEquals("2A01", r.getID());
+    }
+
+    @Test
+    public void testGetRoom_invalid_throws() {
+        Organization org = new Organization();
+        try {
+            org.getRoom("X999");
+            fail("Should throw for invalid room");
+        } catch (Exception expected) {}
+    }
+
+    @Test
+    public void testGetEmployee_valid() throws Exception {
+        Organization org = new Organization();
+        Person p = org.getEmployee("Greg Gay");
+        assertEquals("Greg Gay", p.getName());
+    }
+
+    @Test
+    public void testGetEmployee_invalid_throws() {
+        Organization org = new Organization();
+        try {
+            org.getEmployee("Unknown Person");
+            fail("Should throw for invalid employee");
+        } catch (Exception expected) {}
+    }
 }
