3bd163f65b6b48c3d1963b59e3f2592a15204ee9
Zorigt Munkhbaatar <mzorigt6@gmail.com>
2025-12-12 17:47:38 +0800

    Add multiplication method documentation

--- lab14/src/main/java/lab14/sict/must/edu/mn/Multiplication.java
+++ lab14/src/main/java/lab14/sict/must/edu/mn/Multiplication.java
@@ -2,6 +2,9 @@ package lab14.sict.must.edu.mn;
 
 public class Multiplication {
 
+  /**
+   * Multiply two operands and return the raw product.
+   */
   public double multiply(double left, double right) {
     return left * right;
   }
