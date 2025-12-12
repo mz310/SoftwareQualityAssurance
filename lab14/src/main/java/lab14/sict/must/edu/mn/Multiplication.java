c9fc389698d79cb4b992a235698b0e5501c518da
Zorigt Munkhbaatar <mzorigt6@gmail.com>
2025-12-12 17:47:59 +0800

    Document multiplication parameters on develop

--- lab14/src/main/java/lab14/sict/must/edu/mn/Multiplication.java
+++ lab14/src/main/java/lab14/sict/must/edu/mn/Multiplication.java
@@ -2,6 +2,13 @@ package lab14.sict.must.edu.mn;
 
 public class Multiplication {
 
+  /**
+   * Multiplies two numbers using straightforward arithmetic.
+   *
+   * @param left left operand
+   * @param right right operand
+   * @return product of both operands
+   */
   public double multiply(double left, double right) {
     return left * right;
   }
